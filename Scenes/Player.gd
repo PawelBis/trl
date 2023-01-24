d9227e0ef300b13c271ddb152c610f4bdeb9a715
Jarosław Bandosz <jaroslaw.bandosz@gmail.com>
2023-01-24 17:50:13 +0100

    Basic movement (including diagonals) added.

---
+++ Scenes/Player.gd
@@ -0,0 +1,45 @@
+extends Node
+
+var baseMoveSpeed = 100
+var currentMoveSpeed = 100
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func get_input():
+	if Input.is_action_just_pressed("Up"):
+		_tryToMove(0, -16, false)
+	if Input.is_action_just_pressed("Down"):
+		_tryToMove(0, 16, false)
+	if Input.is_action_just_pressed("Left"):
+		_tryToMove(-16, 0, false)
+	if Input.is_action_just_pressed("Right"):
+		_tryToMove(16, 0, false)
+	if Input.is_action_just_pressed("UpLeft"):
+		_tryToMove(-16, -16, true)
+	if Input.is_action_just_pressed("DownLeft"):
+		_tryToMove(-16, +16, true)
+	if Input.is_action_just_pressed("UpRight"):
+		_tryToMove(16, -16, true)
+	if Input.is_action_just_pressed("DownRight"):
+		_tryToMove(16, 16, true)
+		
+func _physics_process(_delta):
+	get_input()
+	
+func _tryToMove(x, y, diagonal):
+	if !diagonal:
+		$Sprite.position.x = $Sprite.position.x + x
+		$Sprite.position.y = $Sprite.position.y + y
+	if diagonal:
+		$Sprite.position.x = $Sprite.position.x + x
+		$Sprite.position.y = $Sprite.position.y + y
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
