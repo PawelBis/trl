22ca8d2b6f58cf73742fee9f89b23439adc4ed72
Jarosław Bandosz <jaroslaw.bandosz@gmail.com>
2023-01-25 18:59:17 +0100

    Enemies now will act if they have enough time units stored in theirs pools. Only one action per player action for now, gonna fix that later.

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -2,6 +2,7 @@ extends Node
 
 var baseMoveSpeed = 150
 var currentMoveSpeed = baseMoveSpeed
+var attackSpeed = 50
 
 var timeUnitsPool = 0
 
@@ -18,14 +19,20 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Player_command_executed(cost):
-	#
+	#try to attack the player if they are in melee range, move toward them if else
 	if isPlayerInMeleeRange:
-		_attack_Player(cost)
+		_try_to_attack_Player(cost)
 	else:
 		_move_towards_Player(cost)
 
 func _move_towards_Player(cost):
 	print("moving towards player")
 	
-func _attack_Player(cost):
-	print("attacking player")
+func _try_to_attack_Player(cost):
+	if cost + timeUnitsPool >= attackSpeed:
+		print("attacking player")
+		timeUnitsPool = (timeUnitsPool + cost) - attackSpeed
+		print("pool: " + str(timeUnitsPool))
+	else:
+		timeUnitsPool += cost
+		print("storing time units in pool")
